16e7c36d5f27d45cc449d379fc98522da39e115a
krest <18635572970@163.com>
2023-04-01 18:34:23 +0800

    first commit

---
+++ app/src/main/java/com/krest/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.krest.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.krest.app.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/krest/app/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.krest.app.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
